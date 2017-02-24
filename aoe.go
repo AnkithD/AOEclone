49a08ffa606b3a17f082fa60ebceb8ab25560796
dhtikna <dhtikna@yahoo.com>
2017-02-24 15:58:37 +0530

    initialized the engo main file

---
+++ aoe.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"engo.io/ecs"
+	"engo.io/engo"
+	"engo.io/engo/common"
+
+	"image/color"
+	"log"
+)
+
+type myScene struct{}
+
+func (*myScene) Type() string { return "myGame" }
+
+func (*myScene) Preload() {
+
+}
+
+func (*myScene) Setup(world *ecs.World) {
+	world.AddSystem(new(common.RenderSystem))
+
+	comm
+}
+func main() {
+	opts := engo.RunOptions{
+		Title:  "AOE Clone",
+		Width:  1280,
+		Height: 720,
+	}
+
+	engo.Run(opts, new(myScene))
+}
